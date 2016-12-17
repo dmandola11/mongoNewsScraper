169e2b15206221a1a931243e7220ee6febf6c273
Damian Mandola <dmandola11@gmail.com>
2016-12-17 17:34:33 -0600

    added models

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create a schema class
+var Schema = mongoose.Schema;
+
+// Create the Comment schema
+var CommentSchema = new Schema({
+  // Just a string
+  title: {
+    type: String
+  },
+  // Just a string
+  body: {
+    type: String
+  }
+});
+
+
+// Create the Comment model with the CommentSchema
+var Comment = mongoose.model("Comment", CommentSchema);
+
+// Export the Note model
+module.exports = Comment;
---
+++ server.js
@@ -0,0 +1,38 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var Comment = require("./models/Comment.js");
+var Article = require("./models/Article.js");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/week18day3mongoose");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
